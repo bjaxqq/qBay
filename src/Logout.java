1c67873bde1240f85b3752ed5bf3b4b0d9f8d8da
pkwiatkowski-ssj <pkwiatkowski@quinnipiac.edu>
2025-03-30 19:52:26 -0400

    added logout feature

---
+++ src/Logout.java
@@ -0,0 +1,12 @@
+// Logout.java
+package src;
+
+public class Logout {
+    public static void handleLogout() {
+        
+        System.out.println("\nThank you for using qBay!");
+        System.out.println("You have successfully logged out.");
+
+        System.exit(0);
+    }
+}
